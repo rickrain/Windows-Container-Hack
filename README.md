d081e1cac9c63407d687adb19963423833abe10d
Rick Rainey <rickra@microsoft.com>
2018-04-05 14:47:43 -0500

    Added cluster deployment instructions.

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Windows-Container-Hack
+
+## Description
+This repository contains scripts and artifacts to deploy a mixed cluster (Windws and Linux) using the [Azure acs-engine](https://github.com/Azure/acs-engine).  The cluster will consist of two agent pools where each pool contains two virtual machines.  One pool will consist of Windows virtual machines, and the other will consist of Linux/Ubuntu virtual machines.  The master node will be configured with Kubernete v1.10.
+
+The resources used are as follows:
+*  `deploy-acs.ps1` : PowerShell script that will deploy the cluster as described in the `kubernetes.json` file. 
+* `kubernetes.json` : JSON file describing the cluster to be deployed by acs-engine.
+* `acs-engine.exe` : The acs-engine reads the `kubernetes.json` file and generates the Azure Resource Manager (ARM) template, template parameter file, and other artifacts to deploy the cluster.  The files generated will be placed in an `_output` folder where `acs-engine.exe` exists on your machine.  The script will start a new Azure resource group deployment using the files generated under the `_output` folder.
+
+** This has been tested with [Windows acs-engine v0.15.0](https://github.com/Azure/acs-engine/releases/tag/v0.15.0).**
+
+## Requirements
+* Azure subscription.
+* PowerShell 3.0 or higher.
+* Azure PowerShell Cmdlets, which can be downloaded and installed from [here](https://azure.microsoft.com/en-us/downloads/).
+* Windows version of the Azure acs-engine, which can be downloaded from [here](https://github.com/Azure/acs-engine/releases/tag/v0.15.0).
+* Code editor of your choice, such as [Visual Studio Code](https://code.visualstudio.com/Download).
+
+## Instructions
+
+1. Open `deploy-acs.ps1`.  Replace the variables at the top of the script with values from your environment.
+   * `$subscriptionId` : Your Azure subscription Id.
+   * `$tenantId` : The tenant / directory Id for your Azure subscription.
+   * `$resourceGroupName` : The name of the resource group to deploy the cluster into.  This can be a new or existing resource group.
+   * `$resourceGroupLocation` : The location you want to deploy the cluster in.  For example, _eastus_, _westus_, etc.
+   * `$adminUserName` : The user name you want to use for the administrator profile on the Windows and Linux nodes.  This is also the administrator profile that will be used for the master node.
+   * `$adminPassword` : The password that will be used to sign into the Windows agent pool nodes only.  The Linux agent pool nodes and master node will use the SSH key to sign-in.
+   * `$sshPublicKey` : The SSH public key.  Make sure to leave the "ssh-rsa " string at the beginning.
+   * `$acsEnginePath` : Full path to the `acs-engine.exe` file.
+
+   An example showing the variables populated is shown here:
+   ![sample script output](./images/script-variables.png)
+2. Run the script.
+   * Note: The script will prompt you to authenticate to your Azure subscription.  You will also see a console window launch periodically when the script invokes acs-engine.exe to generate the ARM template and artifacts.
+
+   The output for the script will look similar to the output shown here:
+   ![sample script output](./images/script-output.png)
---
+++ deploy-acs.ps1
@@ -0,0 +1,83 @@
+﻿# Variables used below
+$subscriptionId = "<Azure Subscription Id>"
+$tenantId = "<Azure Tenant Id>"
+$resourceGroupName = "<Resource Group Name>"
+$resourceGroupLocation = "<Region>"
+$adminUserName = "<Admin User>"
+$adminWindowsPassword = "<Admin Password>"
+$sshPublicKey="ssh-rsa <public key>"
+$acsEnginePath="<path to acs-engine.exe>"
+
+#################################################################
+# Don't modify anything below unless you know what you're doing.
+#################################################################
+$ErrorActionPreference = "Stop"
+$WarningPreference = "SilentlyContinue"
+
+$kubernetesJsonPath="$PSScriptRoot\kubernetes.json"
+
+$azureSession = Login-AzureRmAccount -Subscription $subscriptionId -TenantId $tenantId
+Write-Host "Creating resource group '$resourceGroupName' in '$resourceGroupLocation'" -ForegroundColor Yellow
+$resourceGroup = New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Force
+$ticks = [DateTime]::UtcNow.Ticks
+
+$clusterDNSName = $($resourceGroup.ResourceGroupName) + "-" + $ticks
+
+# Create an Azure AD App registration
+$azureADAppName = "$($resourceGroup.ResourceGroupName)-" + $ticks
+$azureADAppHomePage = "https://" + $azureADAppName
+Write-Host "Creating an Azure AD application registration '$azureADAppName'" -ForegroundColor Yellow
+$azureADApp = New-AzureRmADApplication -DisplayName $azureADAppName -HomePage $azureADAppHomePage `
+                -IdentifierUris $azureADAppHomePage
+
+# Create a new client secret / credential for the Azure AD App registration
+$bytes = New-Object Byte[] 32
+$rand = [System.Security.Cryptography.RandomNumberGenerator]::Create()
+$rand.GetBytes($bytes)
+$clientSecret = [System.Convert]::ToBase64String($bytes)
+$clientSecretSecured = ConvertTo-SecureString -String $clientSecret -AsPlainText -Force
+$endDate = [System.DateTime]::Now.AddYears(1)
+Write-Host "- Adding a client secret / credential for the application" -ForegroundColor Yellow
+New-AzureRmADAppCredential -ApplicationId $azureADApp.ApplicationId -Password $clientSecretSecured -EndDate $endDate
+
+# Create an Azure AD Service Principal associated with the Azure AD App
+Write-Host "Creating service principal for Azure AD application '$($azureADApp.ApplicationId)'" -ForegroundColor Yellow
+$azureADSP = New-AzureRmADServicePrincipal -ApplicationId $azureADApp.ApplicationId
+
+# Need to pause after creaeting the service principal or you may get an error on the next call indicating the SP doesn't exist.
+Start-Sleep -Seconds 60
+
+# Assign the service principal to the Contributor role for the resource group
+Write-Host "Adding service principal '$($azureADSP.Id)' to the Contributor role for the resource group." -ForegroundColor Yellow
+New-AzureRmRoleAssignment -RoleDefinitionName "Contributor" `
+    -Scope "/subscriptions/$subscriptionId/resourcegroups/$($resourceGroup.ResourceGroupName)" -ObjectId $azureADSP.Id
+
+# Create a new cluster definition file
+$clusterJson = Get-Content $kubernetesJsonPath | Out-String | ConvertFrom-Json
+$clusterJson.properties.masterProfile.dnsPrefix = $clusterDNSName
+$clusterJson.properties.servicePrincipalProfile.clientId = $azureADSP.Id
+$clusterJson.properties.servicePrincipalProfile.secret = $clientSecret
+$clusterJson.properties.windowsProfile.adminUserName = $adminUserName
+$clusterJson.properties.windowsProfile.adminPassword = $adminWindowsPassword
+$clusterJson.properties.linuxProfile.adminUsername = $adminUserName
+$clusterJson.properties.linuxProfile.ssh.publicKeys[0].keyData = $sshPublicKey
+
+$kubernetesJsonFileName = Split-Path $kubernetesJsonPath -Leaf
+$newKubernetesJsonFile = "new_$kubernetesJsonFileName"
+$newKubernetesJsonPath = (Split-Path $kubernetesJsonPath) + "\$newKubernetesJsonFile"
+
+$clusterJson | ConvertTo-Json -Depth 100 -Compress | Out-File $PSScriptRoot\$newKubernetesJsonFile -Encoding ascii
+$acsEngineOutputPath = (Split-Path $acsEnginePath) + "\_output\$clusterDNSName"
+
+# Execute acs-engine to generate deployment templates and artifacts
+Write-Host "Executing acs-engine to generate cluster deployment templates and artifacts." -ForegroundColor Yellow
+$acsEngineArgs = "generate $newKubernetesJsonPath --output-directory $acsEngineOutputPath"
+Start-Process -FilePath $acsEnginePath -ArgumentList $acsEngineArgs -Wait
+
+# Start a new resource group deployment
+Write-Host "Deploying mixed cluster to Azure in resource group '$resourceGroupName'" -ForegroundColor Yellow
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroup.ResourceGroupName `
+    -TemplateFile "$acsEngineOutputPath\azuredeploy.json" `
+    -TemplateParameterFile "$acsEngineOutputPath\azuredeploy.parameters.json"
+
+Write-Host "Successfully deployed!" -ForegroundColor Green
